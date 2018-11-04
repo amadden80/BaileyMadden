f15cfd045f39b9288f531e6036fe1f63f44986d9
Andrew Madden <amadden80@gmail.com>
2018-11-04 10:41:12 -0500

    begin actual website work

--- scripts.js
+++ scripts.js
@@ -1,78 +1,82 @@
-function generateBoxOfBars(numVert, numHorz) {
-  var verts = (numVert + "").split("").map(d => parseInt(d));
-  var horzs = (numHorz + "").split("").map(d => parseInt(d));
-
-  const box = document.createElement("div");
-  box.className = "box";
-
-  for (let i = 0; i < verts.length; i++) {
-    const numVert = verts[i];
-    const vertBox = document.createElement("div");
-    vertBox.className = "vert-box";
-    vertBox.style = `width:${100 / verts.length}%`;
-    for (let i = 0; i < numVert; i++) {
-      const bar = document.createElement("div");
-      bar.className = "vert-bar";
-
-      setTimeout(() => {
-        vertBox.appendChild(bar);
-      }, i * 100);
-    }
-    box.appendChild(vertBox);
-  }
-
-  const horzBox = document.createElement("div");
-  horzBox.className = "horz-box";
-
-  for (let i = 0; i < horzs.length; i++) {
-    const numhorz = horzs[i];
-    const horzBox = document.createElement("div");
-    horzBox.className = "horz-box";
-    horzBox.style = `height:${100 / horzs.length}%;top:${(100 / horzs.length) *
-      i}%`;
-    for (let i = 0; i < numhorz; i++) {
-      const bar = document.createElement("div");
-      bar.className = "horz-bar";
-      setTimeout(() => {
-        horzBox.appendChild(bar);
-      }, i * 100);
-    }
-    box.appendChild(horzBox);
-  }
-  return box;
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
 
-window.onload = function() {
-  // Lines
-  const body = document.querySelector("body");
+var getTopBottom = function(el) {
+  var rect = el.getBoundingClientRect();
+  var top = rect.top;
+  var bottom = rect.bottom;
+  return {
+    top: top,
+    bottom: bottom
+  };
+};
 
-  const linesGroups = [
-    generateBoxOfBars(6, 11),
-    generateBoxOfBars(7, 8),
-    generateBoxOfBars(19, 15)
-  ];
-  linesGroups.forEach(group => {
-    body.appendChild(group);
-  });
+var isBetween = function(value, top, bottom) {
+  return value > top && value < bottom;
+};
 
-  setTimeout(() => {
-    linesGroups.forEach(group => {
-      body.removeChild(group);
-    });
+var isVisable = function(el) {
+  var elTB = getTopBottom(el);
 
-    const circle = document.createElement("div");
-    circle.className = "circle";
+  var viewport = document.documentElement.clientHeight;
 
-    function circleTextLine(text) {
-      const circleT = document.createElement("p");
-      circleT.className = "circle-text";
-      circleT.innerText = text;
-      return circleT;
+  return isBetween(elTB.top, viewport * -0.9, viewport * 0.9);
+};
+
+let discoverTab = function(dot) {
+  [".welcome", ".guide", ".where", ".registry"].forEach(function(section) {
+    if (isVisable(document.body.querySelector(`.stage ${section}`))) {
+      var vTb = getTopBottom(
+        document.body.querySelector(`.navigation ${section}`)
+      );
+      var color;
+      switch (section) {
+        case ".welcome":
+          color = "#dfe3eb";
+          break;
+        case ".where":
+          color = "#f5918e";
+          break;
+        case ".guide":
+          color = "#f8bf9d";
+          break;
+        case ".registry":
+          color = "#697e95";
+          break;
+        default:
+          break;
+      }
+
+      dot.style.borderColor = `transparent ${color} transparent transparent`;
+
+      dot.style.top = vTb.top + "px";
     }
+  });
+};
 
-    circle.appendChild(circleTextLine("Save The Date"));
-    circle.appendChild(circleTextLine("6.7.19"));
+window.onload = function() {
+  var dot = document.body.querySelector(".navigation .dot");
 
-    body.appendChild(circle);
-  }, 5000);
+  discoverTabDebounced = debounce(function() {
+    discoverTab(dot);
+  }, 100);
+
+  //   window.onscroll = function() {
+  //     discoverTabDebounced();
+  //   };
+
+  setInterval(discoverTabDebounced, 250);
 };
